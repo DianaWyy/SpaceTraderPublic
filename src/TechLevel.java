85d401a309988b08aa4793f707ba7727803121b0
TigerSHe1998 <55221117+TigerSHe1998@users.noreply.github.com>
2019-10-02 17:01:10 -0400

    enum class for TechLevel

---
+++ src/TechLevel.java
@@ -0,0 +1,39 @@
+/**
+ * This enum represents all the 7 possible TechLevels for a region
+ * @author Haorong She
+ * @version 0.1
+ */
+public enum TechLevel {
+
+    PREAG("Pre-Agriculture", 1), AGRICULTURE("Agriculture", 2), MEDIEVAL("Medieval", 3),
+    RENAISSANCE("Renaissance", 4), INDUSTRIAL("Industrial", 5), MODERN("Modern", 6),
+    FUTURISTIC("Futuristic", 7);
+
+
+    private String name;
+    private int level;
+
+    /**
+     * Constructor for TechLevel, private only
+     */
+    private TechLevel(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    /**
+     * TechLevel name getter method
+     * @return the TechLevel name in string
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * TechLevel integer getter method
+     * @return the TechLevel in int
+     */
+    public int getLevel() {
+        return this.level;
+    }
+}
