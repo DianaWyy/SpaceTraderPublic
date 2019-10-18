11864abc39a69bcbc74fa79b5d1e10944cf96af9
Frankxhy <hxu389@gatech.edu>
2019-10-18 14:35:39 -0400

    Update Market.java

--- src/Market.java
+++ src/Market.java
@@ -2,6 +2,38 @@ import java.util.ArrayList;
 
 public class Market {
     public Item[] allItems = Item.values();
+    ArrayList<Item> effectiveItems = new ArrayList<>();
+    TechLevel techLevel;
+
+    public Market(TechLevel techLevel) {
+        for(int i = 0; i < allItems.length; i++) {
+            if (allItems[i].getTechlevel() == techLevel) {
+                effectiveItems.add(allItems[i]);
+            }
+        }
+    }
+
+
+    public void buy(Item item, Player player){
+        if(player.getCredits() < item.getPrice()) {
+            throw new IllegalArgumentException("Could not afford");
+        }
+
+        player.setCredits(player.getCredits() - item.getPrice());
+    }
+
+    /**
+     * Should be affected based on the Player’s Merchant skill.
+     * You should be able to explain and demonstrate the effect of the Player’s Merchant skill on prices.
+     * Can start a new game with a different Merchant skill to demonstrate.
+     * The effect should be noticeable.
+     * Use a price calculator service here - you would need to create this.
+     * In case of Python, an appropriate utility function would fulfill this requirement.
+     * In case of Java Swing, an instance method for the Region class would fulfil this requirement.
+     */
+
+
+
 
 
 
