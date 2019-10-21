db08f704dfb7239d4b00cd3d32b800317ef83f3f
DianaWyy <dannawyy@gmail.com>
2019-10-21 10:16:54 -0400

    update checkstyle

--- src/RegionPage.java
+++ src/RegionPage.java
@@ -4,10 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.util.ArrayList;
 
 public class RegionPage extends JFrame {
-
     private Player p;
     private Ship ship;
     private Item selectedItem;
@@ -27,12 +25,9 @@ public class RegionPage extends JFrame {
         ship = p.getShip();
         JFrame f = new JFrame("Region");
         Market market = new Market(r.getTechlevel(), p);
-        ArrayList<Item> marketItemList = market.getItemList();
-        // get width and height of screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = screenSize.width;
         int height = screenSize.height;
-
         f.setSize(width, height);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -41,44 +36,36 @@ public class RegionPage extends JFrame {
         f.getContentPane().setForeground(new Color(25, 25, 25));
         f.getContentPane().setBackground(new Color(25, 25, 25));
         f.setLayout(null);
-
-
-        // show credits
-
         showCredits = new JLabel("Credits： " + p.getCredits());
         showCredits.setBounds(width - 300, 100, 400, 75);
         showCredits.setFont(new Font("Serif", Font.BOLD, 24));
         showCredits.setForeground(Color.white);
         f.add(showCredits);
-
-        showCargoSpace = new JLabel(String.format("Cargo Space: %d/%d", ship.getCurrCargoSpace(), ship.getCargoSpace()));
+        showCargoSpace = new JLabel(String.format("Cargo Space: %d/%d",
+                ship.getCurrCargoSpace(), ship.getCargoSpace()));
         showCargoSpace.setBounds(width - 300, 150, 400, 75);
         showCargoSpace.setFont(new Font("Serif", Font.BOLD, 24));
         showCargoSpace.setForeground(Color.white);
         f.add(showCargoSpace);
-
-        welcome = new JLabel(String.format("Welcome to Planet %s at (%d, %d)", r.getName(), r.getXCoordinate(),
-                r.getYCoordinate()));
+        welcome = new JLabel(String.format("Welcome to Planet %s at (%d, %d)",
+                r.getName(), r.getXCoordinate(), r.getYCoordinate()));
         welcome.setAlignmentX(Component.CENTER_ALIGNMENT);
         welcome.setBounds(width / 2 - 100, height / 6, 400, 50);
         welcome.setFont(new Font("Serif", Font.BOLD, 24));
         welcome.setForeground(Color.white);
         f.add(welcome);
-
-        techLevel = new JLabel(String.format("This Region is in the %s Era", r.getTechlevel().getName()));
+        techLevel = new JLabel(String.format("This Region is in the %s Era",
+                r.getTechlevel().getName()));
         techLevel.setAlignmentX(Component.CENTER_ALIGNMENT);
         techLevel.setBounds(width / 2 - 100, height / 6 + 50, 400, 50);
         techLevel.setFont(new Font("Serif", Font.BOLD, 24));
         techLevel.setForeground(Color.white);
         f.add(techLevel);
-
         message = new JLabel("");
         message.setBounds(width / 2 - 100, height / 6 + 150, 400, 50);
         message.setFont(new Font("Serif", Font.BOLD, 24));
         message.setForeground(Color.white);
         f.add(message);
-
-        // Add Market Drop-down
         buyDD = new JComboBox(market.getItemNameList().toArray());
         buyDD.setSelectedIndex(0);
         selectedItem = marketItemList.get(buyDD.getSelectedIndex());
@@ -95,20 +82,17 @@ public class RegionPage extends JFrame {
         );
         buyDD.setBounds(width / 2 - 100, height / 6 + 200, 250, 25);
         f.add(buyDD);
-
         buy = new JButton("Buy");
         buy.setFont(new Font("Serif", Font.BOLD, 20));
         buy.setBounds(width / 2 + 200, height / 6 + 200, 100, 40);
         f.add(buy);
-
         buy.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                if(p.getCredits() < selectedItem.getPrice()) {
+                if (p.getCredits() < selectedItem.getPrice()) {
                     message.setText("you are too pOoR");
                     return;
                 }
-
                 if (ship.getCurrCargoSpace() == p.getShip().getCargoSpace()) {
                     message.setText("your ship is too sMaLl");
                     return;
@@ -117,15 +101,14 @@ public class RegionPage extends JFrame {
                 p.setCredits(p.getCredits() - selectedItem.getPrice());
                 message.setText("Thanks for purchasing:)");
                 showCredits.setText("Credits： " + p.getCredits());
-                showCargoSpace.setText(String.format("Cargo Space: %d/%d", ship.getCurrCargoSpace(), ship.getCargoSpace()));
+                showCargoSpace.setText(String.format("Cargo Space: %d/%d",
+                        ship.getCurrCargoSpace(), ship.getCargoSpace()));
                 sellDD.setModel(new DefaultComboBoxModel(ship.getCargoNameList().toArray()));
                 ItemEvent event = new ItemEvent(sellDD, 0, null, ItemEvent.SELECTED);
                 ItemListener l = sellDD.getItemListeners()[0];
                 l.itemStateChanged(event);
             }
         });
-
-        // Add Market Drop-down
         sellDD = new JComboBox(ship.getCargoNameList().toArray());
         if (ship.getCargoList().size() != 0) {
             sellDD.setSelectedIndex(0);
@@ -149,11 +132,10 @@ public class RegionPage extends JFrame {
         sell.setFont(new Font("Serif", Font.BOLD, 20));
         sell.setBounds(width / 2 + 200, height / 6 + 300, 100, 40);
         f.add(sell);
-
         sell.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                if(ship.getCargoList().size() == 0) {
+                if (ship.getCargoList().size() == 0) {
                     message.setText("you got nothin' boi");
                     return;
                 }
@@ -161,7 +143,8 @@ public class RegionPage extends JFrame {
                 p.setCredits(p.getCredits() + selectedCargo.getPrice());
                 message.setText("you got some $$$:)");
                 showCredits.setText("Credits： " + p.getCredits());
-                showCargoSpace.setText(String.format("Cargo Space: %d/%d", ship.getCurrCargoSpace(), ship.getCargoSpace()));
+                showCargoSpace.setText(String.format("Cargo Space: %d/%d",
+                        ship.getCurrCargoSpace(), ship.getCargoSpace()));
                 sellDD.setModel(new DefaultComboBoxModel(ship.getCargoNameList().toArray()));
                 if (ship.getCargoList().size() != 0) {
                     sellDD.setSelectedIndex(0);
@@ -169,15 +152,11 @@ public class RegionPage extends JFrame {
                 }
             }
         });
-
-
-        // Add View Map Button
         map = new JButton("View Map"); //creating instance of JButton
         map.setAlignmentX(Component.CENTER_ALIGNMENT);
         map.setFont(new Font("Serif", Font.BOLD, 20));
         map.setBounds(width / 2, height / 4 + 400, 150, 40);
         f.add(map);
-
         map.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 f.dispose();
@@ -185,7 +164,6 @@ public class RegionPage extends JFrame {
             }
         });
     }
-
     private void updateCargoList() {
 
     }
