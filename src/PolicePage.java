265dd58b72bd81bfca3b2084c3cf1d5ea1ea4658
DianaWyy <dannawyy@gmail.com>
2019-11-06 16:56:31 -0500

    fix Checkstyle

--- src/PolicePage.java
+++ src/PolicePage.java
@@ -10,51 +10,35 @@ public class PolicePage {
 
 
     public PolicePage(Player p, Ship s, Region selected, double fuel, MapPage mp, int numCargo) {
-        //put into instance variables
         this.p = p;
         this.s = s;
-
-        // Figure out stolen goods
         Item[] stolen = new Item[numCargo];
-
-
         Collections.shuffle(s.getCargoList());
-
         for (int i = 0; i < numCargo; i++) {
             stolen[i] = s.getCargoList().get(i);
             stolenList += stolen[i] + ", ";
         }
-
-        //UI layout
         JFrame frame = new JFrame("Police Encountered!");
         frame.setSize(1280, 720);
         frame.setLayout(new BorderLayout());
         JPanel jp = new JPanel();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true); // full screen
-
         Box b = new Box(BoxLayout.Y_AXIS);
-
-
-        // TODO
-        // Fix the Orientation somehow
         JLabel message = new JLabel("A police squadron has appeared!");
         JLabel message2 = new JLabel("The chief demands you hand over some stolen goods");
         JLabel message3 = new JLabel("The following goods are marked as stolen: ");
         JLabel message4 = new JLabel(stolenList);
-
         JLabel banditIcon = new JLabel(new ImageIcon("pic/police.png"));
         JButton forfeit = new JButton("FORFEIT");
         JButton flee = new JButton("FLEE");
         JButton fight = new JButton("FIGHT");
         JButton negotiate = new JButton("negotiate");
         JButton leave = new JButton("Keep Traveling..");
-
         b.add(message, BorderLayout.CENTER);
         b.add(message2, BorderLayout.CENTER);
         b.add(message3, BorderLayout.CENTER);
         b.add(message4, BorderLayout.CENTER);
-
         frame.add(banditIcon, BorderLayout.NORTH);
         frame.add(b, BorderLayout.CENTER);
         jp.add(forfeit);
@@ -62,19 +46,14 @@ public class PolicePage {
         jp.add(fight);
         jp.add(negotiate);
         negotiate.setEnabled(false);
-
         frame.add(jp, BorderLayout.SOUTH);
         frame.setBounds(300, 200, 600, 300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        //action events
         forfeit.addActionListener(l -> {
-
             for (Item i : stolen) {
                 s.getCargoList().remove(i);
             }
-
             message.setText("The POPO thank you for your cooperation!");
             message2.setText("The following items have been confiscated!");
             message3.setText(stolenList);
@@ -83,7 +62,6 @@ public class PolicePage {
             frame.add(leave, BorderLayout.SOUTH);
             p.setCurrentRegion(selected);
             s.decreaseCurrFuelCapacity(fuel);
-
         });
         flee.addActionListener(l -> {
             if (p.getPilotSkill() / 20.0 > Math.random()) {
@@ -95,19 +73,15 @@ public class PolicePage {
                 frame.add(leave, BorderLayout.SOUTH);
             } else {
                 s.decreaseCurrFuelCapacity(fuel);
-
                 int beforeHealth = s.getCurrHealth();
                 s.setCurrHealth(Math.max(s.getCurrHealth() - 20, 0));
                 int afterHealth = s.getCurrHealth();
-
                 int beforeCredits = p.getCredits();
                 p.setCredits(beforeCredits / 2);
                 int afterCredits = p.getCredits();
-
                 for (Item i : stolen) {
                     s.getCargoList().remove(i);
                 }
-
                 message.setText(
                         "You failed to run away!"
                                 + " The police fined you half of your credits and hit your ship!");
@@ -122,7 +96,6 @@ public class PolicePage {
             if (p.getFighterSkill() / 20.0 > Math.random()) {
                 p.setCurrentRegion(selected);
                 s.decreaseCurrFuelCapacity(fuel);
-
                 message.setText("You completely destroyed the POPO!");
                 message2.setText("You decide to continue along your way.");
                 message3.setText("");
@@ -132,19 +105,15 @@ public class PolicePage {
             } else {
                 p.setCurrentRegion(selected);
                 s.decreaseCurrFuelCapacity(fuel);
-
                 int beforeHealth = s.getCurrHealth();
                 s.setCurrHealth(Math.max(s.getCurrHealth() - 20, 0));
                 int afterHealth = s.getCurrHealth();
-
                 int beforeCredits = p.getCredits();
                 p.setCredits(beforeCredits / 2);
                 int afterCredits = p.getCredits();
-
                 for (Item i : stolen) {
                     s.getCargoList().remove(i);
                 }
-
                 message.setText(
                         "You failed to beat the POPO!"
                                 + " The police fined you half of your credits and hit your ship!");
